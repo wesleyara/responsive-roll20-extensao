57adc30c970e6ff58bd30f2b4619b5897f657797
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2021-11-27 19:33:48 -0300

    organizando repositório

---
+++ content.js
@@ -0,0 +1,11 @@
+
+onload = function() {
+    var sideBar = document.querySelector("#rightsidebar");
+    var menuList = document.querySelector(".tabmenu.ui-tabs-nav.ui-helper-reset.ui-helper-clearfix.ui-widget-header.ui-corner-all")
+    var textChat = document.querySelector("#textchat-input")
+
+    
+    textChat.style.cssText = `
+    width: 100px;
+    `
+}
